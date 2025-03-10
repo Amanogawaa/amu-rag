6566067de1f47a1c0172a3bb02467c4d8b593437
Dominic Molino <dommolino28@gmail.com>
2025-03-11 02:20:52 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<main
+	class="w-full min-h-svh h-full mx-auto flex flex-col items-center p-10 justify-center bg-primary-d"
+>
+	<div class=" max-w-4xl mx-auto w-full flex-grow">
+		<div class="chat chat-start">
+			<div class="chat-bubble rounded-full bg-tertiary-d">
+				It's over Anakin,
+				<br />
+				I have the high ground.
+			</div>
+		</div>
+		<div class="chat chat-end">
+			<div class="chat-bubble bg-tertiary-d">You underestimate my power!</div>
+		</div>
+	</div>
+	<div
+		class="w-full flex flex-col max-w-4xl mx-auto items-center bg-secondary-d px-2 py-5 rounded-4xl"
+	>
+		<div class="w-full">
+			<input
+				type="text"
+				placeholder="Type here"
+				class="input w-full border-none focus:border-none focus:outline-none focus:ring-0 active:border-none active:outline-0 active:ring-0 bg-transparent text-white outline-0 ring-0"
+			/>
+		</div>
+	</div>
+</main>
